9562b15d67d0a1c9afdbbddfca8592880b2444c9
Anthony Senior <webdevelopment@flareintl.com>
2026-04-06 21:04:37 -0300

    Remove Sanity Studio from build (too heavy for 8GB Vercel), keep lightweight data fetching only

--- src/sanity/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-import homepage from "./homepage";
-import aboutPage from "./aboutPage";
-import court from "./court";
-import galleryPhoto from "./galleryPhoto";
-import forumCategory from "./forumCategory";
-import siteSettings from "./siteSettings";
-
-export const schemaTypes = [siteSettings, homepage, aboutPage, court, galleryPhoto, forumCategory];
